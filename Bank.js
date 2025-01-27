705506b709b032e0f4413cc7666c8fa771690c10
faiq-bokhari <faiq_bokhari@hotmail.com>
2025-01-27 10:23:02 -0500

    Added method for creating account in Bank Class and Added utility methods in Account Class

--- Bank.js
+++ Bank.js
@@ -7,6 +7,11 @@ class Bank {
 
     // Add methods here:
     // Example: createAccount(name, initialDeposit)
+    createAccount(name, initialDeposit){
+        const account = new Account(name, initialDeposit);
+        this.accounts.push(account);
+        return account;
+    }
 
 }
 
@@ -21,16 +26,45 @@ class Account {
     // Add methods here:
     // Example: deposit(amount) 
     // example data to be stored in transactionHistory { transactionType: 'Deposit', amount: 500 }
+    deposit(amount){
+        this.balance += amount;
+        this.transactionHistory.push({'transactionType': 'Deposit', 'amount': amount });
+    }
 
     // Example: withdraw(amount)
     // example data to be stored in transactionHistory { transactionType: 'Withdrawal', amount: 200 }
+    withdraw(amount){
+        if(this.balance >= amount){
+            this.balance -= amount;
+            this.transactionHistory.push({'transactionType': 'Withdrawal', 'amount': amount });
+        } else {
+            console.log("Insufficient Funds");
+            return;
+        }
+    }
 
     // Example: transfer(amount, recipientAccount)
     // example data to be stored in transactionHistory:
     // for account sending { transactionType: 'Transfer', amount: 300, to: recipientName }
     // for account recieving { transactionType: 'Received', amount: 300, from: senderName }
+    transfer(amount, recipientAccount){
+        if(this.balance >= amount){
+            this.balance -= amount;
+            this.transactionHistory.push({'transactionType': 'Transfer', 'amount': amount, 'to': recipientAccount.name});
+    
+            recipientAccount.balance += amount;
+            recipientAccount.transactionHistory.push({'transactionType': 'Received', 'amount': amount, 'from': this.name});
+        } else {
+            console.log("Insufficient Funds");
+            return;
+        }
+
+    }
     
     // Example: checkBalance()
+    checkBalance(){
+        return this.balance;
+    }
 }
 
 //<-------------------------------DO NOT WRITE BELOW THIS LINE------------------------------>
